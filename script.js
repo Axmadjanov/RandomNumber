a7be458cea8ed93940c374c6bc16dfb54fa238f8
Ahror <Ahror@Lenovo.com>
2022-03-12 21:00:24 +0300

    auto counter added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bar = document.querySelector(".bar");
-const exit = document.querySelector(".exit");
 const leftCotainer = document.querySelector(".leftCotainer");
 const shadowNav = document.querySelector(".shadowNav");
+const counters = document.querySelectorAll(".counter");
 
 bar.addEventListener("click", () => {
   leftCotainer.classList.remove("hidden");
@@ -9,6 +9,19 @@ bar.addEventListener("click", () => {
 shadowNav.addEventListener("click", () => {
   leftCotainer.classList.add("hidden");
 });
-exit.addEventListener("click", () => {
-  leftCotainer.classList.add("hidden");
+
+// Counter Part
+
+counters.forEach((counter) => {
+  counter.innerHTML = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerHTML;
+    const increment = target / 200;
+    if (c < target) {
+      counter.innerHTML = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1.5);
+    }
+  };
+  updateCounter();
 });
