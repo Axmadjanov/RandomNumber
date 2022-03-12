aad26cf52afc9f6957b5d0b1eccf0a6ed0bc6540
Ahror <Ahror@Lenovo.com>
2022-03-12 19:34:24 +0300

    hi

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-const randomNum = document.querySelector(".randomNum1")
-const container= document.querySelector(".container")
-const checkButton = document.querySelector(".checkButton")
-const number = document.querySelector(".number")
-const Guessing = document.querySelector(".guessing")
-const resultScore1 = document.querySelector(".resultScore1")
-const highScore1 = document.querySelector(".highScore1")
+const bar = document.querySelector(".bar");
+const exit = document.querySelector(".exit");
+const leftCotainer = document.querySelector(".leftCotainer");
+const shadowNav = document.querySelector(".shadowNav");
 
-randomNum.textContent=Math.floor(Math.random() * 10)
-
-
-checkButton.addEventListener("click",()=>{
-    if(number.value === randomNum.textContent){
-         Guessing.textContent="Corret number";
-container.style.background="yellowgreen"
-    }
-    if( number.value > randomNum.textContent ){
-            Guessing.textContent="Too high";
-    resultScore1.textContent --;
-    }
-    if(number.value < randomNum.textContent){
-            Guessing.textContent="Too low";
-    resultScore1.textContent --;
-    }
-
-
-})
+bar.addEventListener("click", () => {
+  leftCotainer.classList.remove("hidden");
+});
+shadowNav.addEventListener("click", () => {
+  leftCotainer.classList.add("hidden");
+});
+exit.addEventListener("click", () => {
+  leftCotainer.classList.add("hidden");
+});
